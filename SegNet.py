aad0a3f55f1358d5ed4dfd8dac4f1df6cb799138
legokichi <legokichi@gmail.com>
2017-04-11 12:02:11 +0000

    add args

--- SegNet.py
+++ SegNet.py
@@ -63,7 +63,7 @@ class DePool2D(UpSampling2D):
         return f
 
 
-def create_segnet(shape: Tuple[int,int,int], nb_class: int, indecis: bool) -> tModel :
+def create_segnet(shape: Tuple[int,int,int], nb_class: int, indices: bool) -> tModel :
     # base on https://github.com/alexgkendall/SegNet-Tutorial/blob/master/Models/segnet_train.prototxt
     # and https://github.com/alexgkendall/SegNet-Tutorial/blob/master/Example_Models/segnet_model_driving_webdemo.prototxt
 
@@ -86,30 +86,30 @@ def create_segnet(shape: Tuple[int,int,int], nb_class: int, indecis: bool) -> tM
     x = encoder.output
 
     # Block 5
-    if indecis: x = DePool2D(L[0], size=L[0].pool_size, input_shape=encoder.output_shape[1:])(x)
+    if indices: x = DePool2D(L[0], size=L[0].pool_size, input_shape=encoder.output_shape[1:])(x)
     else:       x = UpSampling2D(  size=L[0].pool_size, input_shape=encoder.output_shape[1:])(x)
     x = Activation('relu')(BatchNormalization()(Conv2D(L[1].filters, L[1].kernel_size, padding=L[1].padding, kernel_initializer="he_normal", bias_initializer='zeros')(x)))
     x = Activation('relu')(BatchNormalization()(Conv2D(L[2].filters, L[2].kernel_size, padding=L[2].padding, kernel_initializer="he_normal", bias_initializer='zeros')(x)))
     x = Activation('relu')(BatchNormalization()(Conv2D(L[3].filters, L[3].kernel_size, padding=L[3].padding, kernel_initializer="he_normal", bias_initializer='zeros')(x)))
     # Block 4
-    if indecis: x = DePool2D(L[4], size=L[4].pool_size)(x)
+    if indices: x = DePool2D(L[4], size=L[4].pool_size)(x)
     else:       x = UpSampling2D(  size=L[4].pool_size)(x)
     x = Activation('relu')(BatchNormalization()(Conv2D(L[5].filters, L[5].kernel_size, padding=L[5].padding, kernel_initializer="he_normal", bias_initializer='zeros')(x)))
     x = Activation('relu')(BatchNormalization()(Conv2D(L[6].filters, L[6].kernel_size, padding=L[6].padding, kernel_initializer="he_normal", bias_initializer='zeros')(x)))
     x = Activation('relu')(BatchNormalization()(Conv2D(L[7].filters, L[7].kernel_size, padding=L[7].padding, kernel_initializer="he_normal", bias_initializer='zeros')(x)))
     # Block 3
-    if indecis: x = DePool2D(L[8], size=L[8].pool_size)(x)
+    if indices: x = DePool2D(L[8], size=L[8].pool_size)(x)
     else:       x = UpSampling2D(  size=L[8].pool_size)(x)
     x = ZeroPadding2D(padding=(0, 1))(x)
     x = Activation('relu')(BatchNormalization()(Conv2D(L[10].filters, L[10].kernel_size, padding=L[10].padding, kernel_initializer="he_normal", bias_initializer='zeros')(x)))
     x = Activation('relu')(BatchNormalization()(Conv2D(L[11].filters, L[11].kernel_size, padding=L[11].padding, kernel_initializer="he_normal", bias_initializer='zeros')(x)))
     # Block 2
-    if indecis: x = DePool2D(L[12], size=L[12].pool_size)(x)
+    if indices: x = DePool2D(L[12], size=L[12].pool_size)(x)
     else:       x = UpSampling2D(   size=L[12].pool_size)(x)
     x = Activation('relu')(BatchNormalization()(Conv2D(L[13].filters, L[13].kernel_size, padding=L[13].padding, kernel_initializer="he_normal", bias_initializer='zeros')(x)))
     x = Activation('relu')(BatchNormalization()(Conv2D(L[14].filters, L[14].kernel_size, padding=L[14].padding, kernel_initializer="he_normal", bias_initializer='zeros')(x)))
     # Block 1
-    if indecis: x = DePool2D(L[15], size=L[15].pool_size)(x)
+    if indices: x = DePool2D(L[15], size=L[15].pool_size)(x)
     else:       x = UpSampling2D(   size=L[15].pool_size)(x)
     x = Activation('relu')(BatchNormalization()(Conv2D(L[16].filters, L[16].kernel_size, padding=L[16].padding, kernel_initializer="he_normal", bias_initializer='zeros')(x)))
     x = Activation('relu')(BatchNormalization()(Conv2D(L[17].filters, L[17].kernel_size, padding=L[17].padding, kernel_initializer="he_normal", bias_initializer='zeros')(x)))
@@ -128,20 +128,22 @@ def create_segnet(shape: Tuple[int,int,int], nb_class: int, indecis: bool) -> tM
 
 
 
-def train(shape: Tuple[int, int, int], nb_class: int, batch_gen: Iterator[Tuple[np.ndarray, np.ndarray]], class_weight: List[float]) -> tModel :
+def train(shape: Tuple[int, int, int], nb_class: int, batch_gen: Iterator[Tuple[np.ndarray, np.ndarray]], class_weight: List[float], indices: bool) -> tModel :
     if len(class_weight) != nb_class:
         raise TypeError("len(class_weight) != nb_class")
     if shape[2] != 3:
         raise TypeError("shape[2] != 3")
+    name = "segnet"
+    if indices: name = "segnet_indecis"
 
     callbacks = [] # type: List[Callback]
 
     # callbacks.append( ModelCheckpoint("weights.{epoch:02d}-{val_loss:.2f}.hdf5", verbose=1, save_best_only=True, save_weights_only=True) )
     # callbacks.append( __something__tensorboard__ )
 
-    segnet = create_segnet(shape, nb_class, indecis=True)
-    with open('segnet.json', 'w') as f: f.write(segnet.to_json())
-    segnet.save_weights('segnet_weight.hdf5')
+    segnet = create_segnet(shape, nb_class, indices)
+    with open(name+'_model.json', 'w') as f: f.write(segnet.to_json())
+    segnet.save_weights(name+'_weight.hdf5')
     
     hist = segnet.fit_generator(
         batch_gen,
@@ -151,7 +153,7 @@ def train(shape: Tuple[int, int, int], nb_class: int, batch_gen: Iterator[Tuple[
         class_weight=class_weight,
         callbacks=callbacks
     )
-    with open('history.json', 'w') as f: f.write(repr(hist.history))
+    with open(name+'_history.json', 'w') as f: f.write(repr(hist.history))
 
     return segnet
 
@@ -163,7 +165,7 @@ def load() -> tModel :
 
 
 if __name__ == '__main__':
-    segnet = create_segnet((480, 360, 3), 12, indecis=True)
+    segnet = create_segnet((480, 360, 3), 12, indices=False)
     segnet.summary()
     plot_model(segnet, to_file='segnet.png', show_shapes=True, show_layer_names=True)
     exit()
